c6095b26bfcda25368600f226690bcab00f9299f
hkemal <hasankemal96@gmail.com>
2023-05-02 13:09:08 +0300

    Initial commit

---
+++ src/main/java/com/hkemal/simpleinsuraceapp/SimpleInsuraceAppApplication.java
@@ -0,0 +1,13 @@
+package com.hkemal.simpleinsuraceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleInsuraceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleInsuraceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hkemal/simpleinsuraceapp/SimpleInsuraceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkemal.simpleinsuraceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleInsuraceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
